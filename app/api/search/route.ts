d894bf94d8d5e831c50346399e107aafcf39b882
Sohom Pal <sohom377@gmail.com>
2023-08-30 11:43:19 +0530

    implementing oxylabs e-commerce api

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -26,6 +26,32 @@ export async function POST(request: Request) {
                 value: key === 'sort_by' ? value : Number(value)
             })
         }
+    });
+
+    const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Basic ${Buffer.from (
+                process.env.OXYLABS_USERNAME + ":" + process.env.OXYLABS_PASSWORD
+                ).toString("base64")}`,
+        },
+        cache: 'no-store',
+        body: JSON.stringify({
+            source: 'google_shopping_search',
+            domain: "com",
+            query: searchTerm,
+            pages: Number(pages) || 1,
+            parse: true,
+            context: filters,
+        })
     })
+
+    const data = await response.json();
+    // console.log(data)
+    
+    const pageResults: PageResult[] = data.results;
+
+    return NextResponse.json(pageResults)
 
 }
