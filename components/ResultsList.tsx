f54ffc0bbe388c2e1076970e3cb4c6ceaf703814
Sohom Pal <sohom377@gmail.com>
2023-08-30 14:50:05 +0530

    resultsList component

---
+++ components/ResultsList.tsx
@@ -0,0 +1,18 @@
+import { PageResult } from "@/typings";
+import Link from "next/link";
+
+type Props = {
+  results: PageResult[];
+  term: String;
+};
+function ResultsList({ results, term }: Props) {
+  return (
+    <div>
+      {/* sidebar  */}
+
+      {/* main body */}
+    </div>
+  );
+}
+
+export default ResultsList;
