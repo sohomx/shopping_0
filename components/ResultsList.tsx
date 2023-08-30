a73c96bf048ff6085f6bafd2a1049a49261ccb65
Sohom Pal <sohom377@gmail.com>
2023-08-30 17:19:03 +0530

    fetching URLs

--- components/ResultsList.tsx
+++ components/ResultsList.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 type Props = {
   results: PageResult[];
-  term: String;
+  term: string;
 };
 function ResultsList({ results, term }: Props) {
   return (
@@ -32,6 +32,29 @@ function ResultsList({ results, term }: Props) {
         ))}
       </div>
       {/* main body */}
+      <div>
+        {results.map((pageResult, i) => (
+          <div
+            key={pageResult.job_id}
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 "
+          >
+            {i !== 0 && <hr className="w-full col-span-full" />}
+
+            <div className="md:col-span-2 lg:col-span-3 xl:col-span-4 py-5">
+              <div className="flex space-x-2 items-center divide-x-2">
+                <h1>shop on google</h1>
+                <h2 className="text-xl font-semibold pl-2">
+                  Search Results for Page {i + 1}
+                </h2>
+
+                <h3 className="font-extralight">
+                  Showing Results for "{decodeURIComponent(term)}"
+                </h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
