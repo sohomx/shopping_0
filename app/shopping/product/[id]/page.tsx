19f3a75d538d65ec09883d9b04acfd69d91bf4e2
Sohom Pal <sohom377@gmail.com>
2023-08-31 14:35:36 +0530

    building the product page

--- app/shopping/product/[id]/page.tsx
+++ app/shopping/product/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getFetchUrl } from "@/lib/getFetchUrl";
+import { ProductData } from "@/typings";
 import { StarIcon } from "@heroicons/react/24/solid";
 import { notFound } from "next/navigation";
 
@@ -12,9 +13,41 @@ type Props = {
 
 async function ProductPage({ params: { id } }: Props) {
   const response = await fetch(getFetchUrl(`api/shopping/product/${id}`));
+  const productData = (await response.json()) as ProductData;
   // API ENDPOINT
 
-  return <div>ProductPage</div>;
+  if (!productData.content.pricing) {
+    notFound();
+  }
+
+  return (
+    <div className="p-12 pt-0">
+      <h1 className="text-2xl">{productData.content.title}</h1>
+
+      {productData.content.reviews && (
+        <div className="flex space-x-1">
+          {/* renders the actual stars from the review  */}
+          {[
+            ...Array.from({
+              length: Math.round(productData.content.reviews.rating),
+            }),
+          ].map((_, i) => (
+            <StarIcon key={i} className="h-5 w-5 text-yellow-500" />
+          ))}
+
+          {/* show remaining stars from review out of 5  */}
+
+          {[
+            ...Array.from({
+              length: 5 - Math.round(productData.content.reviews.rating),
+            }),
+          ].map((_, i) => (
+            <StarIcon key={i} className="h-5 w-5 text-gray-200" />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ProductPage;
