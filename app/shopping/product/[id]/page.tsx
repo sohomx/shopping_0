a777554f2f88c0ab0f33ee2bcbb06c19eab38cf7
Sohom Pal <sohom377@gmail.com>
2023-08-31 11:18:26 +0530

    product page

---
+++ app/shopping/product/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { notFound } from "next/navigation";
+
+export const revalidate = 300;
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+function ProductPage({ params: { id } }: Props) {
+  return <div>ProductPage</div>;
+}
+
+export default ProductPage;
